ce4ab44df2aeb5a92bd77dda625ce80afabc0fa5
obskur123 <tomasxd768@outlook.com>
2024-07-16 14:27:44 -0300

    arregle un bug: cuando se recagargaba la pagina en el post no se encontraban los estilos

--- src/app.rs
+++ src/app.rs
@@ -34,7 +34,7 @@ pub fn App() -> impl IntoView {
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"/>
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/rust.min.js"/>
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js"/>
-        <Stylesheet id="leptos" href="pkg/blog.css"/>
+        <Stylesheet id="leptos" href="/pkg/blog.css"/>
         // sets the document title
         <Title text="Blog de Tomás"/>
         // content for this welcome page
